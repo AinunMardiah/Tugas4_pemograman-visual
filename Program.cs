e7b2324db641de6d0d00cffda61cd919b015d796
AinunMardiah <85837195+AinunMardiah@users.noreply.github.com>
2022-03-04 21:20:17 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: AINUN HUSIN
+ * Date: 04/03/2022
+ * Time: 21:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tugas_Latihan
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
