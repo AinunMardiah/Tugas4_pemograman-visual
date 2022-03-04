547e64c25d06ba6e5b81a77171f018b0930b242d
AinunMardiah <85837195+AinunMardiah@users.noreply.github.com>
2022-03-04 21:22:27 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,31 @@
 ﻿/*
  * Created by SharpDevelop.
  * User: AINUN HUSIN
- * Date: 04/03/2022
- * Time: 21:12
+ * Date: 02/03/2022
+ * Time: 14:49
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Windows.Forms;
 
-namespace tugas_Latihan
+namespace tugaspertemuan_4
 {
-	class Program
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
 	{
-		public static void Main(string[] args)
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
 		}
+		
 	}
 }
